723b3df02b3f0a0449ea48d886a396a78c4bdebf
QuackityDuck <navaneethnandakumar12@gmail.com>
2025-06-08 22:53:42 +0530

    Initial Commit

---
+++ hookX/hookXApp.swift
@@ -0,0 +1,17 @@
+//
+//  hookXApp.swift
+//  hookX
+//
+//  Created by Navaneeth on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct hookXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
